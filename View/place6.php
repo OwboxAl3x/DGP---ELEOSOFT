679f36f7579bc3c517616e93d20c8f95c7623357
Alvaro Guerrero Pinteño <alvar88@correo.ugr.es>
2018-12-19 22:28:45 +0100

    Update place6.php

--- View/place6.php
+++ View/place6.php
@@ -22,7 +22,7 @@ $logged = true;
     <div class="container">
         <div class="row">
             <div class="col-lg-7">
-                <div class="intro_image"><img src="../Imagenes/veleta1.jpg" width="400" alt=""></div>
+                <div class="intro_image"><img src="../Imagenes/veleta1.jpg" width="400" alt="Foto de veleta"></div>
             </div>
             <div class="col-lg-5">
                 <div class="intro_content">
